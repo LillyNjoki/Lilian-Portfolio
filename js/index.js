938369ff48906e221e51d82ecc250487c26b7e15
lilian <lillyannenjoki@gmail.com>
2022-11-21 03:31:47 +0300

    Portfolio

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const x = document.getElementById("addr");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+}
+
+function changeColor(){
+let newColor = document.querySelectorAll("navbar");
+      for (const color of newColor) {
+        color.addEventListener("mouseover", () => {
+          color.style.backgroundColor = "blue";
+        });
+      }
+}
